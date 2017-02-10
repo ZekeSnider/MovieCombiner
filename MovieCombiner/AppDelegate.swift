20f4f30657cebadae7c35bfd3fae359eef002133
Zeke Snider <zeke.snider.contractor@sonosite.com>
2017-02-09 20:29:38 -0800

    Initial Commit

---
+++ MovieCombiner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MovieCombiner
+//
+//  Created by Zeke Snider on 2/9/17.
+//  Copyright © 2017 Zeke Snider. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MovieCombiner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MovieCombiner
+//
+//  Created by Zeke Snider on 2/9/17.
+//  Copyright © 2017 Zeke Snider. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
